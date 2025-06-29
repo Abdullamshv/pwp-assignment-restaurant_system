9afe3cf8307e272249ae4526f3c5d3e207a1d7fa
YuenNingOng <ningyuen03@gmail.com>
2025-06-30 01:37:06 +0800

    import view active order function

--- users/chef.py
+++ users/chef.py
@@ -1,6 +1,7 @@
 import json
 import os
 import sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from utils.order_management import chef_view_active_orders
 # Ensure the parent directory is in the path for module imports
 project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
