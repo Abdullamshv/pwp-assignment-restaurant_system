f992069933a93217a8727f600de69fac7bd4e827
SEAH YU ZHEN <seahyz0628@gmail.com>
2025-06-30 16:58:42 +0800

    manager

--- users/manager.py
+++ users/manager.py
@@ -7,8 +7,12 @@ from utils.manager_utils import view_all_orders
 from utils.manager_utils import view_customer_feedback
 from utils.manager_utils import track_finances
 from utils.manager_utils import manage_promo_codes
-from .chef import  check_inventory
+from .chef import  check_inventory, load_data
 
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DATA_DIR = os.path.join(project_root, "data")
+RECIPE_FILE = os.path.join(DATA_DIR, "recipe.txt")
+INVENTORY_FILE = os.path.join(DATA_DIR, "inventory.txt")
 
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -34,7 +38,7 @@ def manager_menu():
         elif choice == "3":
             track_finances()
         elif choice == "4":
-            check_inventory()
+            check_inventory(load_data(RECIPE_FILE), load_data(INVENTORY_FILE))
         elif choice == "5":
             view_customer_feedback()
         elif choice == "6":
